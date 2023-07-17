5f3905ed76a0b06f90bf5cc67fb6e884ea992035
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-17 15:33:50 -0700

    created timer_page.dart file which displays the timer as it counts down

---
+++ lib/timer/view/timer_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/bloc/timer_bloc.dart';
+import 'package:flutter_timer/ticker.dart';
+import 'package:flutter_timer/timer/timer.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerText()),
+              ),
+              Actions(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesString =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondsString = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesString:$secondsString',
+      style: Theme.of(context).textTheme.displayLarge,
+    );
+  }
+}
